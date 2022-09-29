57a228544a3d1a506bdff466b29d8ae0556a627d
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-29 16:25:04 +0800

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `Hallo\Kak\n
 *ꜱᴀʏᴀ ᴀᴅᴀʟᴀʜ ᴀʀᴜʟʟʙᴏᴛᴢ ʏᴀɴɢ ʙɪꜱᴀ ᴍᴇᴍʙᴀɴᴛᴜ ᴀɴᴅᴀ ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴜᴀᴛ ꜱᴛɪᴄᴋᴇʀ ᴅᴀɴ ʟᴀɪɴ",ꜱᴀʏᴀ ᴅɪᴄɪᴘᴛᴀᴋᴀɴ\nᴏʟᴇʜ ꜱᴀʜʀᴜʟʟ,ᴜɴᴛᴜᴋ ᴍᴇʟɪʜᴀᴛ ꜰɪᴛᴜʀ ᴀʀᴜʟʟʙᴏᴛᴢ\nꜱɪʟᴀʜᴋᴀɴ ᴘᴇɴᴄᴇᴛ ᴛᴏᴍʙᴏʟ ʙᴜᴛᴛᴏɴ ᴅɪʙᴀᴡᴀʜ 👇* `
-await conn.send2ButtonDoc(m.chat, `${htki} ᴜ s ᴇ ʀ s ${htka}`, info, 'Owner', '.owner', 'Menu', '.m', fgif, adReply)
+await conn.send2ButtonDoc(m.chat, `${htki} ᴜ s ᴇ ʀ s ${htka}`, info, 'Owner', '.owner', 'Menu', '.m', fgif, fakeig)
 await conn.sendMessage(m.chat, {
           react: {
             text: '🥶',
