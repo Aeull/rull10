7b5bff238b5f2d7b249e03beb23dc18c48ca9ead
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-29 16:22:47 +0800

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `Hallo\Kak\n
-*ꜱᴀʏᴀ ᴀᴅᴀʟᴀʜ ᴀʀᴜʟʟʙᴏᴛᴢ ʏᴀɴɢ ʙɪꜱᴀ ᴍᴇᴍʙᴀɴᴛᴜ ᴀɴᴅᴀ ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴜᴀᴛ ꜱᴛɪᴄᴋᴇʀ ᴅᴀɴ ʟᴀɪɴ",ꜱᴀʏᴀ ᴅɪᴄɪᴘᴛᴀᴋᴀɴ ᴏʟᴇʜ ꜱᴀʜʀᴜʟʟ,ᴜɴᴛᴜᴋ ᴍᴇʟɪʜᴀᴛ ꜰɪᴛᴜʀ ᴀʀᴜʟʟʙᴏᴛᴢ ꜱɪʟᴀʜᴋᴀɴ ᴘᴇɴᴄᴇᴛ ᴛᴏᴍʙᴏʟ ʙᴜᴛᴛᴏɴ ᴅɪʙᴀᴡᴀʜ 👇* `
+*ꜱᴀʏᴀ ᴀᴅᴀʟᴀʜ ᴀʀᴜʟʟʙᴏᴛᴢ ʏᴀɴɢ ʙɪꜱᴀ ᴍᴇᴍʙᴀɴᴛᴜ ᴀɴᴅᴀ ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴜᴀᴛ ꜱᴛɪᴄᴋᴇʀ ᴅᴀɴ ʟᴀɪɴ",ꜱᴀʏᴀ ᴅɪᴄɪᴘᴛᴀᴋᴀɴ\nᴏʟᴇʜ ꜱᴀʜʀᴜʟʟ,ᴜɴᴛᴜᴋ ᴍᴇʟɪʜᴀᴛ ꜰɪᴛᴜʀ ᴀʀᴜʟʟʙᴏᴛᴢ\nꜱɪʟᴀʜᴋᴀɴ ᴘᴇɴᴄᴇᴛ ᴛᴏᴍʙᴏʟ ʙᴜᴛᴛᴏɴ ᴅɪʙᴀᴡᴀʜ 👇* `
 await conn.send2ButtonDoc(m.chat, `${htki} ᴜ s ᴇ ʀ s ${htka}`, info, 'Owner', '.owner', 'Menu', '.m', fgif, adReply)
 await conn.sendMessage(m.chat, {
           react: {
