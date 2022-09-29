69113707dc13a94afe4d6cb5a22edb7cfce59cd2
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-29 16:13:36 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -541,7 +541,7 @@ const listMessage = {
 }
 handler.help = ['menu', 'help', '?']
 handler.tags = ['main']
-handler.command = /^(menu|help|\?)$/i
+handler.command = /^(m|help|\?)$/i
 
 handler.register = true
 handler.exp = 3
