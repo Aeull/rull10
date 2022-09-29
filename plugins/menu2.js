0ff4a71e55642cff1bc237644eab3095d49bfa4c
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-29 16:30:42 +0800

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -2,11 +2,11 @@ import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `Hallo\Kak\n
-*ꜱᴀʏᴀ ᴀᴅᴀʟᴀʜ ᴀʀᴜʟʟʙᴏᴛᴢ ʏᴀɴɢ ʙɪꜱᴀ ᴍᴇᴍʙᴀɴᴛᴜ ᴀɴᴅᴀ ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴜᴀᴛ ꜱᴛɪᴄᴋᴇʀ ᴅᴀɴ ʟᴀɪɴ",ꜱᴀʏᴀ ᴅɪᴄɪᴘᴛᴀᴋᴀɴ\nᴏʟᴇʜ ꜱᴀʜʀᴜʟʟ,ᴜɴᴛᴜᴋ ᴍᴇʟɪʜᴀᴛ ꜰɪᴛᴜʀ ᴀʀᴜʟʟʙᴏᴛᴢ\nꜱɪʟᴀʜᴋᴀɴ ᴘᴇɴᴄᴇᴛ ᴛᴏᴍʙᴏʟ ʙᴜᴛᴛᴏɴ ᴅɪʙᴀᴡᴀʜ 👇* `
+*𝐒𝐚𝐲𝐚 𝐀𝐝𝐚𝐥𝐚𝐡 𝐀𝐫𝐮𝐥𝐥𝐁𝐨𝐭𝐳 𝐘𝐚𝐧𝐠 𝐁𝐢𝐬𝐚 𝐌𝐞𝐦𝐛𝐚𝐧𝐭𝐮 𝐀𝐧𝐝𝐚 𝐔𝐧𝐭𝐮𝐤 𝐌𝐞𝐦𝐛𝐮𝐚𝐭 𝐒𝐭𝐢𝐜𝐤𝐞𝐫 𝐃𝐚𝐧 𝐋𝐚𝐢𝐧",𝐒𝐚𝐲𝐚 𝐃𝐢𝐜𝐢𝐩𝐭𝐚𝐤𝐚𝐧 𝐎𝐥𝐞𝐡 𝐒𝐚𝐡𝐫𝐮𝐥𝐥,𝐔𝐧𝐭𝐮𝐤 𝐌𝐞𝐥𝐢𝐡𝐚𝐭 𝐅𝐢𝐭𝐮𝐫 𝐀𝐫𝐮𝐥𝐥𝐁𝐨𝐭𝐳 𝐒𝐢𝐥𝐚𝐡𝐤𝐚𝐧 𝐏𝐞𝐧𝐜𝐞𝐭 𝐓𝐨𝐦𝐛𝐨𝐥 𝐁𝐮𝐭𝐭𝐨𝐧 𝐃𝐢𝐛𝐚𝐰𝐚𝐡👇* `
 await conn.send2ButtonDoc(m.chat, `${htki} ᴜ s ᴇ ʀ s ${htka}`, info, 'Owner', '.owner', 'Menu', '.m', fgif, fakeig)
 await conn.sendMessage(m.chat, {
           react: {
-            text: '🥶',
+            text: '🥶,🗿,🤏,🤖,😭',
             key: m.key,
           }})
 }
